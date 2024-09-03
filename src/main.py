ff25997a560191d43662eb153ec59871ac543dbe
Dounia <benyakhlaf.dounia@gmail.com>
2024-09-03 20:34:18 +0200

    quick test of the Residue class

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+from residue import *
+
 if __name__ == "__main__":
+
+    # =================== Test of Residue class ===================
+    arginine = Residue("R", 3, "P", 4, 7)
+    glycine = Residue("G", 2, "H", 4, 8)
+    alanine = Residue("A", 52, "H", 10, 8)
+    arginine.res_description()
+    glycine.res_description()
+    alanine.res_description()
+    print(arginine.is_connected(glycine))
+    print(arginine.is_connected(alanine))
