e66a0be94d60e2a7404706e9bd373bc2c0ea982a
Dounia <benyakhlaf.dounia@gmail.com>
2024-09-03 21:32:15 +0200

    update of the Residue class test with the addition of the set_coordinate method test

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from residue import *
 if __name__ == "__main__":
 
     # =================== Test of Residue class ===================
+    print("=================== Test of Residue class ===================")
     arginine = Residue("R", 3, "P", 4, 7)
     glycine = Residue("G", 2, "H", 4, 8)
     alanine = Residue("A", 52, "H", 10, 8)
@@ -11,3 +12,5 @@ if __name__ == "__main__":
     alanine.res_description()
     print(arginine.is_connected(glycine))
     print(arginine.is_connected(alanine))
+    alanine.set_coordinate(11, 11)
+    alanine.res_description()
