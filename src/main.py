1092091e439b1742c42aba90b2b1a14154104045
Dounia <benyakhlaf.dounia@gmail.com>
2024-09-03 22:26:03 +0200

    simplification of protein sequences in the main file: amino acid letters are directly represented by H and P (Hydrophobic and Polar)

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
 
     # =================== Test of Residue class ===================
     print("=================== Test of Residue class ===================")
-    arginine = Residue("R", 3, "P", 4, 7)
-    glycine = Residue("G", 2, "H", 4, 8)
-    alanine = Residue("A", 52, "H", 10, 8)
+    arginine = Residue("P", 3, 4, 7)
+    glycine = Residue("H", 2, 4, 8)
+    alanine = Residue("H", 52, 10, 8)
     arginine.res_description()
     glycine.res_description()
     alanine.res_description()
@@ -18,5 +18,5 @@ if __name__ == "__main__":
 
     # =================== Test of Protein class ===================
     print("=================== Test of Protein class ===================")
-    protein_1 = Protein("GGARLF")
+    protein_1 = Protein("HHPHPP")
     protein_1.print_sequence()
